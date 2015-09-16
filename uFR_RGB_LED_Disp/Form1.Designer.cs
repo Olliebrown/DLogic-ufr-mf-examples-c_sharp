098586347733a970af1db2a93b318252647bccc3
developer4 <support@d-logic.rs>
2015-09-16 11:13:51 +0000

    Project is created
    git-svn-id: svn://oldsvnserver/sw_open/ufr/c%23/examples/trunk@55 ca34c7d2-1fbc-e241-8d02-f02e9b27dd3e

---
+++ uFR_RGB_LED_Disp/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace uFR_RGB_LED_Disp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 458);
+            this.Name = "Form1";
+            this.Text = "uFR RGB LED Display Demo v1.0";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
