4dd430c267990a6cdc9ab68a052b3bbb1b05ce35
developer4 <support@d-logic.rs>
2015-12-08 10:53:52 +0000

    Auto-sleep setup and example version 1.0 "commit in trunk".
    git-svn-id: svn://oldsvnserver/sw_open/ufr/c%23/examples/trunk@102 ca34c7d2-1fbc-e241-8d02-f02e9b27dd3e

---
+++ ufr_auto_sleep_setup/frmMain.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace uFR_RGB_LED_Disp
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel6 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusReader = new System.Windows.Forms.ToolStripStatusLabel();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.tbDeviceSerialNr = new System.Windows.Forms.TextBox();
+            this.lbDeviceSerialNr = new System.Windows.Forms.Label();
+            this.tbDeviceType = new System.Windows.Forms.TextBox();
+            this.lbDeviceType = new System.Windows.Forms.Label();
+            this.numAutoSleepTime = new System.Windows.Forms.NumericUpDown();
+            this.lbAutoSleepTime = new System.Windows.Forms.Label();
+            this.btnSetAutoSleep = new System.Windows.Forms.Button();
+            this.chkEnableAutoSleep = new System.Windows.Forms.CheckBox();
+            this.lbSeconds = new System.Windows.Forms.Label();
+            this.btnWakeUp = new System.Windows.Forms.Button();
+            this.btnSleep = new System.Windows.Forms.Button();
+            this.statusStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAutoSleepTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.AutoSize = false;
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel6,
+            this.statusReader});
+            this.statusStrip.Location = new System.Drawing.Point(0, 216);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(484, 22);
+            this.statusStrip.TabIndex = 23;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // toolStripStatusLabel6
+            // 
+            this.toolStripStatusLabel6.AutoSize = false;
+            this.toolStripStatusLabel6.BorderSides = ((System.Windows.Forms.ToolStripStatusLabelBorderSides)((((System.Windows.Forms.ToolStripStatusLabelBorderSides.Left | System.Windows.Forms.ToolStripStatusLabelBorderSides.Top) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Right) 
+            | System.Windows.Forms.ToolStripStatusLabelBorderSides.Bottom)));
+            this.toolStripStatusLabel6.Name = "toolStripStatusLabel6";
+            this.toolStripStatusLabel6.Size = new System.Drawing.Size(52, 17);
+            this.toolStripStatusLabel6.Text = "STATUS";
+            // 
+            // statusReader
+            // 
+            this.statusReader.AutoSize = false;
+            this.statusReader.Name = "statusReader";
+            this.statusReader.Size = new System.Drawing.Size(360, 17);
+            this.statusReader.Text = "Ok";
+            this.statusReader.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Enabled = false;
+            this.btnClose.Location = new System.Drawing.Point(329, 41);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(129, 23);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Close Reader";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(329, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(129, 23);
+            this.btnOpen.TabIndex = 8;
+            this.btnOpen.Text = "Open Reader";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // tbDeviceSerialNr
+            // 
+            this.tbDeviceSerialNr.Location = new System.Drawing.Point(104, 38);
+            this.tbDeviceSerialNr.Name = "tbDeviceSerialNr";
+            this.tbDeviceSerialNr.ReadOnly = true;
+            this.tbDeviceSerialNr.Size = new System.Drawing.Size(196, 20);
+            this.tbDeviceSerialNr.TabIndex = 3;
+            // 
+            // lbDeviceSerialNr
+            // 
+            this.lbDeviceSerialNr.AutoSize = true;
+            this.lbDeviceSerialNr.Location = new System.Drawing.Point(14, 41);
+            this.lbDeviceSerialNr.Name = "lbDeviceSerialNr";
+            this.lbDeviceSerialNr.Size = new System.Drawing.Size(84, 13);
+            this.lbDeviceSerialNr.TabIndex = 2;
+            this.lbDeviceSerialNr.Text = "Device Serial Nr";
+            this.lbDeviceSerialNr.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // tbDeviceType
+            // 
+            this.tbDeviceType.Location = new System.Drawing.Point(104, 12);
+            this.tbDeviceType.Name = "tbDeviceType";
+            this.tbDeviceType.ReadOnly = true;
+            this.tbDeviceType.Size = new System.Drawing.Size(196, 20);
+            this.tbDeviceType.TabIndex = 1;
+            // 
+            // lbDeviceType
+            // 
+            this.lbDeviceType.AutoSize = true;
+            this.lbDeviceType.Location = new System.Drawing.Point(14, 15);
+            this.lbDeviceType.MinimumSize = new System.Drawing.Size(84, 13);
+            this.lbDeviceType.Name = "lbDeviceType";
+            this.lbDeviceType.Size = new System.Drawing.Size(84, 13);
+            this.lbDeviceType.TabIndex = 0;
+            this.lbDeviceType.Text = "Device Type";
+            this.lbDeviceType.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // numAutoSleepTime
+            // 
+            this.numAutoSleepTime.Enabled = false;
+            this.numAutoSleepTime.Location = new System.Drawing.Point(155, 95);
+            this.numAutoSleepTime.Maximum = new decimal(new int[] {
+            254,
+            0,
+            0,
+            0});
+            this.numAutoSleepTime.Name = "numAutoSleepTime";
+            this.numAutoSleepTime.ReadOnly = true;
+            this.numAutoSleepTime.Size = new System.Drawing.Size(53, 20);
+            this.numAutoSleepTime.TabIndex = 17;
+            // 
+            // lbAutoSleepTime
+            // 
+            this.lbAutoSleepTime.AutoSize = true;
+            this.lbAutoSleepTime.Enabled = false;
+            this.lbAutoSleepTime.Location = new System.Drawing.Point(14, 97);
+            this.lbAutoSleepTime.MinimumSize = new System.Drawing.Size(84, 13);
+            this.lbAutoSleepTime.Name = "lbAutoSleepTime";
+            this.lbAutoSleepTime.Size = new System.Drawing.Size(135, 13);
+            this.lbAutoSleepTime.TabIndex = 16;
+            this.lbAutoSleepTime.Tag = "";
+            this.lbAutoSleepTime.Text = "Time to enter in auto-sleep:";
+            this.lbAutoSleepTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnSetAutoSleep
+            // 
+            this.btnSetAutoSleep.Enabled = false;
+            this.btnSetAutoSleep.Location = new System.Drawing.Point(329, 92);
+            this.btnSetAutoSleep.Name = "btnSetAutoSleep";
+            this.btnSetAutoSleep.Size = new System.Drawing.Size(129, 23);
+            this.btnSetAutoSleep.TabIndex = 18;
+            this.btnSetAutoSleep.Tag = "";
+            this.btnSetAutoSleep.Text = "Set Auto-sleep mode";
+            this.btnSetAutoSleep.UseVisualStyleBackColor = true;
+            this.btnSetAutoSleep.Click += new System.EventHandler(this.btnSetAutoSleep_Click);
+            // 
+            // chkEnableAutoSleep
+            // 
+            this.chkEnableAutoSleep.AutoSize = true;
+            this.chkEnableAutoSleep.Enabled = false;
+            this.chkEnableAutoSleep.Location = new System.Drawing.Point(17, 72);
+            this.chkEnableAutoSleep.Name = "chkEnableAutoSleep";
+            this.chkEnableAutoSleep.Size = new System.Drawing.Size(140, 17);
+            this.chkEnableAutoSleep.TabIndex = 24;
+            this.chkEnableAutoSleep.Text = "Enable auto-sleep mode";
+            this.chkEnableAutoSleep.UseVisualStyleBackColor = true;
+            this.chkEnableAutoSleep.CheckedChanged += new System.EventHandler(this.chkEnableAutoSleep_CheckedChanged);
+            // 
+            // lbSeconds
+            // 
+            this.lbSeconds.AutoSize = true;
+            this.lbSeconds.Enabled = false;
+            this.lbSeconds.Location = new System.Drawing.Point(214, 97);
+            this.lbSeconds.MinimumSize = new System.Drawing.Size(84, 13);
+            this.lbSeconds.Name = "lbSeconds";
+            this.lbSeconds.Size = new System.Drawing.Size(84, 13);
+            this.lbSeconds.TabIndex = 25;
+            this.lbSeconds.Tag = "";
+            this.lbSeconds.Text = "seconds";
+            this.lbSeconds.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnWakeUp
+            // 
+            this.btnWakeUp.Enabled = false;
+            this.btnWakeUp.Location = new System.Drawing.Point(329, 177);
+            this.btnWakeUp.Name = "btnWakeUp";
+            this.btnWakeUp.Size = new System.Drawing.Size(129, 23);
+            this.btnWakeUp.TabIndex = 26;
+            this.btnWakeUp.Tag = "";
+            this.btnWakeUp.Text = "Wake up";
+            this.btnWakeUp.UseVisualStyleBackColor = true;
+            this.btnWakeUp.Click += new System.EventHandler(this.btnWakeUp_Click);
+            // 
+            // btnSleep
+            // 
+            this.btnSleep.Enabled = false;
+            this.btnSleep.Location = new System.Drawing.Point(329, 148);
+            this.btnSleep.Name = "btnSleep";
+            this.btnSleep.Size = new System.Drawing.Size(129, 23);
+            this.btnSleep.TabIndex = 27;
+            this.btnSleep.Tag = "";
+            this.btnSleep.Text = "Sleep";
+            this.btnSleep.UseVisualStyleBackColor = true;
+            this.btnSleep.Click += new System.EventHandler(this.btnSleep_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 238);
+            this.Controls.Add(this.btnSleep);
+            this.Controls.Add(this.btnWakeUp);
+            this.Controls.Add(this.lbSeconds);
+            this.Controls.Add(this.chkEnableAutoSleep);
+            this.Controls.Add(this.btnSetAutoSleep);
+            this.Controls.Add(this.lbAutoSleepTime);
+            this.Controls.Add(this.numAutoSleepTime);
+            this.Controls.Add(this.tbDeviceSerialNr);
+            this.Controls.Add(this.lbDeviceSerialNr);
+            this.Controls.Add(this.tbDeviceType);
+            this.Controls.Add(this.lbDeviceType);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.statusStrip);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "uFR Auto Sleep Setup v1.0";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmMain_FormClosing);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAutoSleepTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel6;
+        private System.Windows.Forms.ToolStripStatusLabel statusReader;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox tbDeviceSerialNr;
+        private System.Windows.Forms.Label lbDeviceSerialNr;
+        private System.Windows.Forms.TextBox tbDeviceType;
+        private System.Windows.Forms.Label lbDeviceType;
+        private System.Windows.Forms.NumericUpDown numAutoSleepTime;
+        private System.Windows.Forms.Label lbAutoSleepTime;
+        private System.Windows.Forms.Button btnSetAutoSleep;
+        private System.Windows.Forms.CheckBox chkEnableAutoSleep;
+        private System.Windows.Forms.Label lbSeconds;
+        private System.Windows.Forms.Button btnWakeUp;
+        private System.Windows.Forms.Button btnSleep;
+    }
+}
+
---
+++ ufr_auto_sleep_setup/frmMain.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using DL_uFCoder;
+
+namespace uFR_RGB_LED_Disp
+{
+    public partial class frmMain : Form
+    {
+        UInt32 tmpAutoSleepTime = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            DL_STATUS status;
+            bool reader_connected;
+            ulong reader_type;
+            byte[] reader_sn = new byte[8];
+
+            statusReader.Text = "Please wait...";
+            this.Update();
+
+            tbDeviceType.Text = "";
+            tbDeviceSerialNr.Text = "";
+            tbDeviceType.Refresh();
+            tbDeviceSerialNr.Refresh();
+
+            status = uFCoder.ReaderOpen();
+
+            reader_connected = status == DL_STATUS.UFR_OK;
+
+            unsafe
+            {
+                status = uFCoder.GetReaderType(&reader_type);
+            }
+            reader_connected &= status == DL_STATUS.UFR_OK;
+
+            unsafe
+            {
+                fixed (byte* rdsn = reader_sn)
+                    status = uFCoder.GetReaderSerialDescription(rdsn);
+            }
+            reader_connected &= status == DL_STATUS.UFR_OK;
+
+            if (!reader_connected)
+            {
+                statusReader.Text = "Reader not connected";
+                return;
+            }
+            else
+            {
+                byte tmp;
+                status = uFCoder.AutoSleepGet(out tmp);
+                if (status == DL_STATUS.UFR_OK)
+                {
+                    if (tmp == 0xFF)
+                        tmp = 0;
+                    tmpAutoSleepTime = tmp;
+                    if ((tmp != 0) && (tmp != 0xFF))
+                    {
+                        chkEnableAutoSleep.Checked = true;
+                    }
+                }
+
+                statusReader.Text = "Reader connected";
+                tbDeviceType.Text = Convert.ToString((long)reader_type, 16).ToUpper();
+                tbDeviceSerialNr.Text = System.Text.Encoding.UTF8.GetString(reader_sn);
+
+                btnOpen.Enabled = false;
+                btnSleep.Enabled = true;
+                btnWakeUp.Enabled = true;
+                btnClose.Enabled = true;
+                chkEnableAutoSleep.Enabled = true;
+                btnSetAutoSleep.Enabled = true;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DL_STATUS status;
+
+            status = uFCoder.ReaderClose();
+
+            if (status == DL_STATUS.UFR_OK)
+            {
+                tbDeviceSerialNr.Text = "";
+                tbDeviceType.Text = "";
+                statusReader.Text = "Reader not connected";
+                chkEnableAutoSleep.Checked = false;
+                chkEnableAutoSleep.Enabled = false;
+                btnSetAutoSleep.Enabled = false;
+                btnSleep.Enabled = false;
+                btnWakeUp.Enabled = false;
+                btnClose.Enabled = false;
+                btnOpen.Enabled = true;
+            }
+        }
+
+        private void chkEnableAutoSleep_CheckedChanged(object sender, EventArgs e)
+        {
+            statusReader.Text = "";
+            if (chkEnableAutoSleep.Checked)
+            {
+                lbAutoSleepTime.Enabled = true;
+                lbSeconds.Enabled = true;
+                numAutoSleepTime.Enabled = true;
+                numAutoSleepTime.Value = tmpAutoSleepTime;
+            }
+            else
+            {
+                lbAutoSleepTime.Enabled = false;
+                lbSeconds.Enabled = false;
+                numAutoSleepTime.Enabled = false;
+                tmpAutoSleepTime = (UInt32)numAutoSleepTime.Value;
+                numAutoSleepTime.Value = 0;
+            }
+        }
+
+        private void frmMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (btnClose.Enabled)
+            {
+                btnClose_Click(sender, e);
+            }
+        }
+
+        private void btnSetAutoSleep_Click(object sender, EventArgs e)
+        {
+            DL_STATUS status;
+
+            statusReader.Text = "Please wait...";
+            status = uFCoder.AutoSleepSet((byte)numAutoSleepTime.Value);
+            if (status == DL_STATUS.UFR_OK)
+            {
+                if ((byte)numAutoSleepTime.Value == 0)
+                {
+                    statusReader.Text = "Auto-sleep mode successfuly disabled";
+                }
+                else
+                {
+                    statusReader.Text = "Auto-sleep mode set successfuly";
+                }
+            }
+            else
+            {
+                statusReader.Text = "Error: " + status.ToString();
+            }
+        }
+
+        private void btnSleep_Click(object sender, EventArgs e)
+        {
+            DL_STATUS status;
+
+            statusReader.Text = "Please wait...";
+            status = uFCoder.UfrEnterSleepMode();
+            if (status == DL_STATUS.UFR_OK)
+            {
+                statusReader.Text = "Device is in sleep mode";
+            }
+            else
+            {
+                statusReader.Text = "Error: " + status.ToString();
+            }
+        }
+
+        private void btnWakeUp_Click(object sender, EventArgs e)
+        {
+            DL_STATUS status;
+
+            statusReader.Text = "Please wait...";
+            status = uFCoder.UfrLeaveSleepMode();
+            if (status == DL_STATUS.UFR_OK)
+            {
+                statusReader.Text = "Device is woken";
+            }
+            else
+            {
+                statusReader.Text = "Error: " + status.ToString();
+            }
+        }
+    }
+}
